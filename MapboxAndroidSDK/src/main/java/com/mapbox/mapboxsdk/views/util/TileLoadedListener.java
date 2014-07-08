98a0754f185fcbcde6a734a9284a5251cd67a013
Brad Leege <bleege@gmail.com>
2014-07-08 14:43:07 -0500

    #417 - Converting to Drawable

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/util/TileLoadedListener.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/util/TileLoadedListener.java
@@ -1,12 +1,7 @@
-/**
- * @author Brad Leege <bleege@gmail.com>
- * Created on 2/5/14 at 8:02 PM
- */
-
 package com.mapbox.mapboxsdk.views.util;
 
-import uk.co.senab.bitmapcache.CacheableBitmapDrawable;
+import android.graphics.drawable.Drawable;
 
 public interface TileLoadedListener {
-    public CacheableBitmapDrawable onTileLoaded(CacheableBitmapDrawable pDrawable);
+    public Drawable onTileLoaded(Drawable pDrawable);
 }
