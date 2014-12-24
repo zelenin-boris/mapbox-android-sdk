adc2c6adaff9bb7032926768df3d1fce8561c0a1
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-12-24 08:33:43 -0800

    added sample OSM data and created a fragment that will be used to display test OSM XML

---
+++ MapboxAndroidSDK/src/main/java/com/spatialdev/osm/OsmXmlParser.java
@@ -0,0 +1,17 @@
+package com.spatialdev.osm;
+
+import android.util.Xml;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Nicholas Hallahan on 12/24/14.
+ * nhallahan@spatialdev.com
+ */
+public class OsmXmlParser {
+
+}
