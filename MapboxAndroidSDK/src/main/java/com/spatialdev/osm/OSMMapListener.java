fe1127b6e55d7e894eb209936ea07584a9a2c4dc
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-01-12 17:20:27 -0800

    drawing tap envelope in debug mode

--- MapboxAndroidSDK/src/main/java/com/spatialdev/osm/OSMMapListener.java
+++ MapboxAndroidSDK/src/main/java/com/spatialdev/osm/OSMMapListener.java
@@ -5,20 +5,29 @@
 
 package com.spatialdev.osm;
 
+import android.graphics.Paint;
+
 import com.mapbox.mapboxsdk.api.ILatLng;
 import com.mapbox.mapboxsdk.events.MapListener;
 import com.mapbox.mapboxsdk.events.RotateEvent;
 import com.mapbox.mapboxsdk.events.ScrollEvent;
 import com.mapbox.mapboxsdk.events.ZoomEvent;
 import com.mapbox.mapboxsdk.overlay.Marker;
+import com.mapbox.mapboxsdk.overlay.PathOverlay;
 import com.mapbox.mapboxsdk.views.MapView;
 import com.mapbox.mapboxsdk.views.MapViewListener;
 import com.spatialdev.osm.model.JTSModel;
+import com.vividsolutions.jts.geom.Envelope;
 
 public class OSMMapListener implements MapViewListener, MapListener {
 
+    // DEBUG MODE - SHOW ENVELOPE AROUND TAP ON MAP
+    private static final boolean DEBUG = true;
+
     private MapView mapView;
     private JTSModel jtsModel;
+
+    private PathOverlay debugTapEnvelopePath;
 
     public OSMMapListener(MapView mapView, JTSModel jtsModel) {
         this.mapView = mapView;
@@ -54,12 +63,46 @@ public class OSMMapListener implements MapViewListener, MapListener {
 
     @Override
     public void onTapMap(MapView pMapView, ILatLng pPosition) {
-        jtsModel.queryWithLatLng(pPosition);
+        float zoom = pMapView.getZoomLevel();
+
+        // DEBUG MODE - SHOW ENVELOPE AROUND TAP ON MAP
+        if (DEBUG) {
+            drawDebugTapEnvelope(pMapView, pPosition, zoom);
+        }
+
+//        jtsModel.queryFromTap(pPosition, zoom);
+    }
+
+    private void drawDebugTapEnvelope(MapView pMapView, ILatLng pPosition, float zoom) {
+        Envelope env = jtsModel.createTapEnvelope(pPosition, zoom);
+        PathOverlay path;
+        if (debugTapEnvelopePath == null) {
+            path = new PathOverlay();
+            debugTapEnvelopePath = path;
+            pMapView.getOverlays().add(path);
+        } else {
+            path = debugTapEnvelopePath;
+        }
+        Paint paint = path.getPaint();
+        paint.setStrokeWidth(0); // hairline mode
+        paint.setARGB(200, 0, 255, 255);
+        double maxX = env.getMaxX();
+        double maxY = env.getMaxY();
+        double minX = env.getMinX();
+        double minY = env.getMinY();
+        path.clearPath();
+        path.addPoint(minY, minX);
+        path.addPoint(maxY, minX);
+        path.addPoint(maxY, maxX);
+        path.addPoint(minY, maxX);
+        path.addPoint(minY, minX);
+        mapView.invalidate();
     }
 
     @Override
     public void onLongPressMap(MapView pMapView, ILatLng pPosition) {
-        jtsModel.queryWithLatLng(pPosition);
+        float zoom = pMapView.getZoomLevel();
+        jtsModel.queryFromTap(pPosition, zoom);
     }
 
     /**
--- MapboxAndroidSDK/src/main/java/com/spatialdev/osm/model/JTSModel.java
+++ MapboxAndroidSDK/src/main/java/com/spatialdev/osm/model/JTSModel.java
@@ -5,11 +5,14 @@
 
 package com.spatialdev.osm.model;
 
+import android.util.Log;
+
 import com.mapbox.mapboxsdk.api.ILatLng;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.index.strtree.STRtree;
 
@@ -17,6 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JTSModel {
+
+    private static final int TAP_PIXEL_TOLERANCE = 24;
+
 
     private ArrayList<OSMDataSet> dataSets;
     private GeometryFactory geometryFactory;
@@ -40,21 +46,42 @@ public class JTSModel {
         addOSMStandaloneNodes(ds);
     }
 
-    public List<OSMElement> queryWithLatLng(ILatLng latLng) {
-        List<OSMElement> matches = new ArrayList<>();
-        double lat = latLng.getLatitude();
-        double lng = latLng.getLongitude();
+    public Envelope createTapEnvelope(ILatLng latLng, float zoom) {
+        return createTapEnvelope(latLng.getLatitude(), latLng.getLongitude(), zoom);
+    }
+
+    public Envelope createTapEnvelope(double lat, double lng, float zoom) {
         Coordinate coord = new Coordinate(lng, lat);
         Envelope envelope = new Envelope(coord);
+
+        // Creating a reasonably sized envelope around the tap location.
+        // Tweak the TAP_PIXEL_TOLERANCE to get a better sized box for your needs.
+        double degreesLngPerPixel = degreesLngPerPixel(zoom);
+        double deltaX = degreesLngPerPixel * TAP_PIXEL_TOLERANCE;
+        double deltaY = scaledLatDeltaForMercator(deltaX, lat);
+        envelope.expandBy(deltaX, deltaY);
+        return envelope;
+    }
+
+    public List<OSMElement> queryFromTap(ILatLng latLng, float zoom) {
+        double lat = latLng.getLatitude();
+        double lng = latLng.getLongitude();
+        Envelope envelope = createTapEnvelope(lat, lng, zoom);
+
+        List<OSMElement> matches = new ArrayList<>();
         List results = rtree.query(envelope);
+
+
+
         for (Object res : results) {
             OSMElement el = (OSMElement) res;
             Geometry geom = el.getJTSGeom();
-            boolean inside = geom.contains(geometryFactory.createPoint(coord));
+            boolean inside = geom.contains(geometryFactory.createPoint(new Coordinate(lng, lat)));
             if (inside) {
                 matches.add(el);
             }
         }
+        Log.i("queryFromTap", matches.toString());
         return matches;
     }
 
@@ -62,14 +89,7 @@ public class JTSModel {
         List<Way> closedWays = ds.getClosedWays();
         for (Way closedWay : closedWays) {
             List<Node> nodes = closedWay.getNodes();
-            Coordinate[] coords = new Coordinate[nodes.size()];
-            int i = 0;
-            for (Node node : nodes) {
-                double lat = node.getLat();
-                double lng = node.getLng();
-                Coordinate coord = new Coordinate(lng, lat);
-                coords[i++] = coord;
-            }
+            Coordinate[] coords = coordArrayFromNodeList(nodes);
             Polygon poly = geometryFactory.createPolygon(coords);
             closedWay.setJTSGeom(poly);
             Envelope envelope = poly.getEnvelopeInternal();
@@ -78,11 +98,60 @@ public class JTSModel {
     }
 
     private void addOSMOpenWays(OSMDataSet ds) {
+        List<Way> openWays = ds.getOpenWays();
+        for (Way w : openWays) {
+            List<Node> nodes = w.getNodes();
+            Coordinate[] coords = coordArrayFromNodeList(nodes);
+            LineString line = geometryFactory.createLineString(coords);
+            w.setJTSGeom(line);
+            Envelope envelope = line.getEnvelopeInternal();
+            rtree.insert(envelope, w);
+        }
+    }
 
+    private Coordinate[] coordArrayFromNodeList(List<Node> nodes) {
+        Coordinate[] coords = new Coordinate[nodes.size()];
+        int i = 0;
+        for (Node node : nodes) {
+            double lat = node.getLat();
+            double lng = node.getLng();
+            Coordinate coord = new Coordinate(lng, lat);
+            coords[i++] = coord;
+        }
+        return coords;
     }
 
     private void addOSMStandaloneNodes(OSMDataSet ds) {
+        List<Node> standaloneNodes = ds.getStandaloneNodes();
+        for (Node n : standaloneNodes) {
 
+        }
+    }
+
+
+    /**
+     * This is how degrees wide a given pixel is for a given zoom.
+     *
+     * @param zoom
+     * @return
+     */
+    private static double degreesLngPerPixel(float zoom) {
+        double degreesPerTile = 360 / Math.pow(2, zoom);
+        return degreesPerTile / 256;
+    }
+
+    /**
+     * This is how many degrees high a given Lat Delta is for a given
+     * zoom in Spherical Mercator.
+     *
+     * http://en.wikipedia.org/wiki/Mercator_projection#Scale_factor
+     *
+     * @param deltaDeg, lng
+     * @return
+     */
+    private static double scaledLatDeltaForMercator(double deltaDeg, double lat) {
+        double scale =  1 / Math.cos(Math.toRadians(lat));
+        return deltaDeg / scale;
     }
 
 }
