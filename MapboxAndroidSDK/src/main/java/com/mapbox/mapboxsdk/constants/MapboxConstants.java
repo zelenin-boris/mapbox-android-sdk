9a9dd09f4a2960155aab49f778152abcdae51adc
Brad Leege <bleege@gmail.com>
2015-01-16 10:43:08 -0600

    #597 - Switching to V4

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
@@ -11,7 +11,7 @@ public interface MapboxConstants {
      * The default base endpoint of Mapbox services.
      * This bakes in a CNAME and version number.
      */
-    public static final String MAPBOX_BASE_URL = "https://a.tiles.mapbox.com/v3/";
+    public static final String MAPBOX_BASE_URL = "https://a.tiles.mapbox.com/v4/";
 
     public static final String USER_AGENT = "Mapbox Android SDK/0.5.1";
 
