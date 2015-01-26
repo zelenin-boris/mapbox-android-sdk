546d853076798e1ad878154dbfce97191ec2c420
Brad Leege <bleege@gmail.com>
2015-01-26 12:08:13 -0600

    #505 - Making ability to custom handle InfoWindow touch events easier by adding InfoWindow.setOnTouchListener().  Updating CustomInfoWindow to show how to make use of it.

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/ui/CustomInfoWindow.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/ui/CustomInfoWindow.java
@@ -1,14 +1,33 @@
 package com.mapbox.mapboxsdk.android.testapp.ui;
 
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.mapbox.mapboxsdk.android.testapp.R;
 import com.mapbox.mapboxsdk.overlay.Marker;
 import com.mapbox.mapboxsdk.views.InfoWindow;
 import com.mapbox.mapboxsdk.views.MapView;
 
 public class CustomInfoWindow extends InfoWindow {
+    
     public CustomInfoWindow(MapView mv) {
         super(R.layout.infowindow_custom, mv);
+        
+        // Add own OnTouchListener to customize handling InfoWindow touch events
+        setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Demonstrate custom onTouch() control
+                Toast.makeText(mView.getContext(), R.string.customInfoWindowOnTouchMessage, Toast.LENGTH_SHORT).show();
+
+                // Still close the InfoWindow though
+                close();
+
+                // Return true as we're done processing this event
+                return true;
+            }
+        });
     }
 
     /**
