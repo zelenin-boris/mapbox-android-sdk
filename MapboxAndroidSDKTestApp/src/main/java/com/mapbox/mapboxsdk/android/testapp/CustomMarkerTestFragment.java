5a1c6ddb01af4d242ce433b07e725c24923062d8
Brad Leege <bleege@gmail.com>
2015-01-26 11:17:39 -0600

    #505 - Fixing assignment of CustomInfoWindow bug

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/CustomMarkerTestFragment.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/CustomMarkerTestFragment.java
@@ -29,9 +29,9 @@ public class CustomMarkerTestFragment extends Fragment {
         Marker capital = new Marker(mv, "Dodoma", "", new LatLng(-6.17691, 35.74685));
         capital.setIcon(new Icon(getActivity(), Icon.Size.LARGE, "town-hall", "FF0000"));
         mv.addMarker(capital);
-        
+
         Marker bigCity = new Marker(mv, "Dar es Salaam", "", new LatLng(-6.80610, 39.27046));
-        marker.setToolTip(new CustomInfoWindow(mv));
+        bigCity.setToolTip(new CustomInfoWindow(mv));
         mv.addMarker(bigCity);
 
         return view;
