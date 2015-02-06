2a9a262f0d1aff0766ffc9e6ad9b9283d3ee42a0
Brad Leege <bleege@gmail.com>
2015-02-06 13:44:45 -0600

    #610 #632 - Adding Constant Interfaces, cleaning up tile string assembly, and formatting for checkstyle.

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/MapTile.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/MapTile.java
@@ -1,8 +1,8 @@
 package com.mapbox.mapboxsdk.tileprovider;
 
 import android.graphics.Rect;
-
 import com.mapbox.mapboxsdk.constants.GeoConstants;
+import com.mapbox.mapboxsdk.constants.MapboxConstants;
 import com.mapbox.mapboxsdk.geometry.BoundingBox;
 import com.mapbox.mapboxsdk.tileprovider.constants.TileLayerConstants;
 
@@ -14,7 +14,7 @@ import com.mapbox.mapboxsdk.tileprovider.constants.TileLayerConstants;
  * {@link com.mapbox.mapboxsdk.overlay.TilesOverlay}). Tiles are typically images (e.g. png or
  * jpeg).
  */
-public class MapTile {
+public class MapTile implements GeoConstants, MapboxConstants, TileLayerConstants {
 
     public static final int MAPTILE_SUCCESS_ID = 0;
     public static final int MAPTILE_FAIL_ID = MAPTILE_SUCCESS_ID + 1;
@@ -30,9 +30,9 @@ public class MapTile {
     private Rect mTileRect;
 
     // For lat/lng bounds calculation
-    private double tileSize = TileLayerConstants.DEFAULT_TILE_SIZE;
-    private double originShift = 2 * Math.PI * GeoConstants.RADIUS_EARTH_METERS / 2.0;
-    private double initialResolution = 2 * Math.PI * GeoConstants.RADIUS_EARTH_METERS / tileSize;
+    private double tileSize = DEFAULT_TILE_SIZE;
+    private double originShift = 2 * Math.PI * RADIUS_EARTH_METERS / 2.0;
+    private double initialResolution = 2 * Math.PI * RADIUS_EARTH_METERS / tileSize;
 
     public MapTile(final int az, final int ax, final int ay) {
         this("", az, ax, ay);
@@ -42,9 +42,7 @@ public class MapTile {
         this.z = az;
         this.x = ax;
         this.y = ay;
-        this.path = String.valueOf(z) + "/" +
-                String.valueOf(x) + "/" +
-                String.valueOf(y);
+        this.path = String.format(MAPBOX_LOCALE, "%d/%d/%d", z, x, y);
         this.cacheKey = aCacheKey + "/" + path;
         this.code = ((17 * (37 + z)) * (37 * x)) * (37 + y);
     }
@@ -102,8 +100,7 @@ public class MapTile {
         return mTileRect;
     }
 
-    public BoundingBox getTileLatLonBounds()
-    {
+    public BoundingBox getTileLatLonBounds() {
         // Returns bounds of the given tile in EPSG:900913 coordinates
         double[] bounds = TileBounds(this.getX(), this.getY(), this.getZ());
         double[] minLatLon = MetersToLatLon(bounds[0], bounds[3]);
@@ -112,36 +109,32 @@ public class MapTile {
         return new BoundingBox(maxLatLon[0], maxLatLon[1], minLatLon[0], minLatLon[1]);
     }
 
-    private double[] TileBounds(int tx, int ty, int zoom)
-    {
+    private double[] TileBounds(int tx, int ty, int zoom) {
         // Returns bounds of the given tile in EPSG:900913 coordinates
         double[] wn = PixelsToMeters(tx * tileSize, ty * tileSize, zoom);
         double[] es = PixelsToMeters((tx + 1) * tileSize, (ty + 1) * tileSize, zoom);
-        return new double[] {wn[0], wn[1], es[0], es[1]};
+        return new double[]{wn[0], wn[1], es[0], es[1]};
     }
 
-    private double[] PixelsToMeters(double px, double py, double zoom)
-    {
+    private double[] PixelsToMeters(double px, double py, double zoom) {
         // Converts pixel coordinates in given zoom level of pyramid to EPSG:900913
         double res = Resolution(zoom);
         double mx = px * res - originShift;
         double my = py * res - originShift;
 
-        return new double[] {mx, my};
+        return new double[]{mx, my};
     }
 
-    private double[] MetersToLatLon(double mx, double my)
-    {
+    private double[] MetersToLatLon(double mx, double my) {
         // Converts XY point from Spherical Mercator EPSG:900913 to lat/lon in WGS84 Datum
         double lon = (mx / originShift) * 180.0;
         double lat = (my / originShift) * 180.0;
-        lat = -180 / Math.PI * (2 * Math.atan( Math.exp( lat * Math.PI / 180.0)) - Math.PI / 2.0);
+        lat = -180 / Math.PI * (2 * Math.atan(Math.exp(lat * Math.PI / 180.0)) - Math.PI / 2.0);
 
-        return new double[] {lat, lon};
+        return new double[]{lat, lon};
     }
 
-    private double Resolution(double zoom)
-    {
+    private double Resolution(double zoom) {
         // Resolution (meters/pixel) for given zoom level (measured at Equator)
         return initialResolution / Math.pow(2, zoom);
     }
